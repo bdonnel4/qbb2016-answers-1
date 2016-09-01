2a7b41ecb4524d782f61d7a2a653a97f67d60373
nshwart1@jhu.edu <cmdb@bio-cmdb-10.local>
2016-09-01 14:58:57 -0400

    add day4 afternoon

---
+++ day4-afternoon/00-scattorPlot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df1 = pd.read_table(sys.argv[1])
+df2 = pd.read_table(sys.argv[2])
+
+
+plt.figure()
+# plt.xscale("log")
+# plt.yscale("log")
+# plt.scatter( df1["FPKM"]+1, df2["FPKM"]+1, alpha=0.1)
+plt.hexbin( df1["FPKM"]+1, df2["FPKM"]+1)
+plt.colorbar()
+plt.savefig("scatterplot.png")
+plt.close()
