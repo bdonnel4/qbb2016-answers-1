b27c2140f9d35fd563a74b6dc3725cf824dc7c14
nshwart1@jhu.edu <cmdb@bio-cmdb-10.local>
2016-09-02 08:49:59 -0400

    add day4-homework complete

--- day4-homework/02-maplot.py
+++ day4-homework/02-maplot.py
@@ -8,37 +8,38 @@ import numpy as np
 df1_ctab=pd.read_csv(sys.argv[1], sep="\t")
 df2_ctab=pd.read_csv(sys.argv[2], sep="\t")
 
-df1_roi = df1_ctab["FPKM"] >= 0
-df2_roi = df2_ctab["FPKM"] >= 0
-
-df1_exp = np.log(df1_ctab[df1_roi]["FPKM"].values)
-df2_exp = np.log(df2_ctab[df2_roi]["FPKM"].values)
+df1_exp = df1_ctab["FPKM"].values
+df2_exp = df2_ctab["FPKM"].values
 
 
-count1 = 0
-count2 = 0
+# count1 = 0
+# count2 = 0
+#
+# for row in df1_exp:
+#     count1 += 1
+# for row in df2_exp:
+#     count2 += 2
+#
+# count_total = count1 + count2
+# count_tot_float = float(count_total)
 
-for row in df1_exp:
-    count1 += 1
-for row in df2_exp:
-    count2 += 2
 
-count_total = count1 + count2
-count_tot_float = float(count_total)
+# avg = (df1_exp+df2_exp)/count_total
+# diff = df2_exp - df1_exp
+#
+# val_avg = np.log(df1_exp)
+# val_diff = np.log(diff)
 
+val_log1 = np.log(df1_exp+1)
+val_log2 = np.log(df2_exp+1)
 
-avg = (df1_exp+df2_exp)/count_total
-diff = df2_exp - df1_exp
-
-val_avg = np.log(df1_exp)
-val_diff = np.log(diff)
-
-# val_log1 = np.log(df1_exp)
-# val_log2 = np.log(df2_exp)
+x = 0.5 * (val_log1 + val_log2)
+y= val_log1 - val_log2
 
 plt.figure()
-plt.scatter(val_log1,val_log2, alpha=0.3)
-plt.scatter(avg,diff)
+plt.scatter(x,y)
+plt.title("FPKM values for SRR072893 and SRR072915")
+plt.xlabel("mean average")
+plt.ylabel("log ratios")
 plt.savefig("ma_plot.png")
-plt.show()
 plt.close()
--- day4-homework/03-densityplot.py
+++ day4-homework/03-densityplot.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
 import sys
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import gaussian_kde
 
-df_ctab = pd.read_csv(sys.argv[1], sep="\t")
+df_ctab = pd.read_table(sys.argv[1])
 
-df_roi = df_ctab["FPKM"] >= 0
-df_high_exp = df_ctab[df_roi]["FPKM"].values
+df1_exp = df_ctab["FPKM"].values
 
-plot.figure()
+density = gaussian_kde(df1_exp)
+
+plt.figure()
 plt.title("Density of SRR072893 FPKM values")
-plt.hexbin(
-            
-)
+xs = np.linspace(np.min(df1_exp),100,100)
+plt.plot(xs,density(xs))
+plt.savefig("density_plot.png")
+plt.close
+
+
